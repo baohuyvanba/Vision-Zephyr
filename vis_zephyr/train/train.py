f9c9e3aa4cbf16b01e4c059162b8a26b7defb528
baohuyvanba <baohuyvanba@gmail.com>
2025-06-27 14:54:02 +0700

    Add LoRA logic

--- vis_zephyr/train/train.py
+++ vis_zephyr/train/train.py
@@ -150,7 +150,7 @@ class TrainingArguments(transformers.TrainingArguments):
     )
 
     #LoRA Arguments
-    lora_enable : bool  = False
+    lora_enable : bool  = False #turn LoRA on or off
     lora_r      : int   = 64
     lora_alpha  : int   = 16
     lora_dropout: float = 0.05
@@ -226,6 +226,7 @@ def find_all_linear_names(model):
             names = name.split('.')
             lora_module_names.add(names[0] if len(names) == 1 else names[-1])
     
+    #Pass lm_head
     if 'lm_head' in lora_module_names:
         lora_module_names.remove('lm_head')
     
@@ -244,18 +245,26 @@ def safe_save_model_for_hf_trainer(
         from vis_zephyr.train.vis_zephyr_trainer import get_mm_adapter_state_maybe_zero
         
         keys_to_match = ['mm_projector']
+        if getattr(trainer.args, "use_im_start_end", False):
+            keys_to_match.extend(['embed_tokens', 'embed_in'])
         weight_to_save = get_mm_adapter_state_maybe_zero(trainer.model.named_parameters(), keys_to_match)
-        # if getattr(trainer.args, "use_im_start_end", False):
-        #     keys_to_match.extend(['embed_tokens', 'embed_in'])
 
+        current_folder = output_dir.split('/')[-1]
+        parent_folder  = os.path.dirname(output_dir)
         if trainer.args.local_rank == 0 or trainer.args.local_rank == -1:
-            os.makedirs(output_dir, exist_ok=True)
-            torch.save(
-                weight_to_save,
-                os.path.join(output_dir, "mm_projector.bin")
-            )
-            rank0_print(f"Saving multimodal projector weights to {os.path.join(output_dir, 'mm_projector.bin')}")
-        
+            if current_folder.startswith('checkpoint-'):
+                mm_projector_model = os.path.join(parent_folder, "mm_projector")
+                os.makedirs(mm_projector_model, exist_ok = True)
+                torch.save(
+                    weight_to_save,
+                    os.path.join(mm_projector_model, f'{current_folder}.bin')
+                )
+                rank0_print(f"Saving multimodal projector weights to {os.path.join(mm_projector_model, f'{current_folder}.bin')}")
+            else:
+                torch.save(
+                    weight_to_save,
+                    os.path.join(output_dir, f'mm_projector.bin')
+                )
         return
     
     #STAGE 2 - Finetuning model: both DeepSpeed and non-Deepspeed)
@@ -676,7 +685,7 @@ def train(
     if model_args.mm_grid_pinpoints is not None:
         rank0_print(f"Using mm_grid_pinpoints: {model_args.mm_grid_pinpoints}")
 
-    # --- 1 --- Set up: Model Loading and Configuration
+    # --- 1 --- Set up: Model Loading and Configuration ------------------------------------------------------------------------------
     #Datatype
     compute_dtype = (
         torch.float16 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else torch.float32)
@@ -698,8 +707,9 @@ def train(
     
     ###HOOK: Quantization training
     
-    # --- 2 --- LoRA Setup
+    # --- 2 --- LoRA Setup (if enabled) ----------------------------------------------------------------------------------------------
     if training_args.lora_enable:
+        from peft import LoraConfig, get_peft_model
         rank0_print("Enabling LoRA for the model.")
         lora_config = LoraConfig(
             r              = training_args.lora_r,
@@ -712,7 +722,7 @@ def train(
         model = get_peft_model(model, lora_config)
         model.print_trainable_parameters()
 
-    # --- 3 --- Set up tokenizer
+    # --- 3 --- Set up tokenizer -----------------------------------------------------------------------------------------------------
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         model_args.model_name_or_path,
         cache_dir        = training_args.cache_dir,
@@ -725,7 +735,7 @@ def train(
     
     conv_lb.default_conversation = conv_lb.templates[model_args.version]
 
-    # --- 4 --- Initialize Vision modules
+    # --- 4 --- Initialize Vision modules --------------------------------------------------------------------------------------------
     model.get_model().initialize_vision_modules(
         model_args = model_args,
     )
@@ -737,9 +747,9 @@ def train(
     data_args.image_processor = vision_tower.image_processor
     data_args.is_multimodal = True
 
-    # --- 5 --- Configs Multimodal Project Training
+    # --- 5 --- Configs Multimodal Project Training ----------------------------------------------------------------------------------
     model.config.tune_mm_mlp_adapter = training_args.tune_mm_mlp_adapter = model_args.tune_mm_mlp_adapter
-    #Training Projector - freeze backbone
+    # > Pretrain: Stage 1
     if model_args.tune_mm_mlp_adapter:
         rank0_print("===== Stage 1: Pre-training Gating Fusion and Multimodal projector =====")
         model.requires_grad_(False)
@@ -753,35 +763,20 @@ def train(
             raise ValueError(
                 "model.get_model().mm_projector is not available."
             )
-    
+    # > Finetune: Stage 2
     #Freeze MLP adapter
     model.config.freeze_mm_mlp_adapter = training_args.freeze_mm_mlp_adapter
     if training_args.freeze_mm_mlp_adapter:
         for p in model.get_model().mm_projector.parameters():
             p.require_grad = False
 
-    # --- 6 --- LoRA Configuration
-    if training_args.lora_enable:
-        rank0_print("Enabling LoRA for the model.")
-        from peft import LoraConfig, get_peft_model
-        lora_config = LoraConfig(
-            r              = training_args.lora_r,
-            lora_alpha     = training_args.lora_alpha,
-            target_modules = find_all_linear_names(model),
-            lora_dropout   = training_args.lora_dropout,
-            bias           = training_args.lora_bias,
-            task_type      = "CAUSAL_LM",
-        )
-        model = get_peft_model(model, lora_config)
-        model.print_trainable_parameters()
-
-    # --- 7 --- Initialize special visual tokens
+    # --- 6 --- Initialize special visual tokens -------------------------------------------------------------------------------------
     model.initialize_vision_tokenizer(
         model_args = model_args,
         tokenizer  = tokenizer,
     )
 
-    # --- 8 --- Set up Data Module & Trainer
+    # --- 7 --- Set up Data Module & Trainer -----------------------------------------------------------------------------------------
     data_module = make_supervised_data_module(
         tokenizer = tokenizer,
         data_args = data_args
@@ -805,7 +800,7 @@ def train(
         rank0_print(f"[BENCH] Trainable params: {n_params_trainable:,}")
     # --- END PREPARE LOGGING ---
 
-    # --- 9 --- TRAINING
+    # --- 8 --- TRAINING -------------------------------------------------------------------------------------------------------------
     if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):
         rank0_print("Resuming training from the last checkpoint.")
         trainer.train(resume_from_checkpoint = True)
@@ -842,10 +837,32 @@ def train(
 
     trainer.save_state()
     model.config.use_cache = True
-    safe_save_model_for_hf_trainer(
-        trainer    = trainer,
-        output_dir = training_args.output_dir
-    )
+
+    # --- 9 --- Save the model -------------------------------------------------------------------------------------------------------
+    if training_args.lora_enable:
+        state_dict = get_peft_state_maybe_zero(
+            model.named_parameters(),
+            training_args.lora_bias
+        )
+        non_lora_state_dict = get_peft_state_non_lora_maybe_zero(
+            model.named_parameters(),
+            require_grad_only = True
+        )
+        if training_args.local_rank == 0 or training_args.local_rank == -1:
+            model.config.save_pretrained(training_args.output_dir)
+            model.save_pretrained(
+                training_args.output_dir,
+                state_dict = state_dict,
+            )
+            torch.save(
+                non_lora_state_dict,
+                os.path.join(training_args.output_dir, "non_lora_trainables.bin")
+            )
+    else:
+        safe_save_model_for_hf_trainer(
+            trainer    = trainer,
+            output_dir = training_args.output_dir
+        )
 
 if __name__ == "__main__":
     train()
