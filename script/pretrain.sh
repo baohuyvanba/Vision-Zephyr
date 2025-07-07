cca0d49a7d8695ab99850569db9d94e1f66432ea
tyzen <62084095+baohuyvanba@users.noreply.github.com>
2025-07-07 20:25:13 +0700

    Update pretrain.sh

--- script/pretrain.sh
+++ script/pretrain.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # =======================================================================================
 #
 #       SCRIPT FOR STAGE 1 PRETRAINING (Vision-Zephyr)
@@ -10,27 +11,27 @@
 #   - Multimodal Projector (MLP) -> Train
 # =======================================================================================
 # deepspeed --include localhost:0,1,2,3 vis_zephyr/train/train_mem.py \
-WANDB_MODE=disabled deepspeed vis_zephyr/train/train_mem.py \
+deepspeed vis_zephyr/train/train_mem.py \
     --deepspeed ./script/zero2.json \
     --tune_mm_mlp_adapter True \
     --mm_projector_lr 2e-3 \
     --model_name_or_path "HuggingFaceH4/zephyr-7b-beta" \
-    --version zephyr_v1 \
-    --data_path ./playground/train_gating_subset_10k.json \
-    --image_folder ./playground/images/train_images_10k \
+    --version plain \
+    --data_path ./playground/data/pretrain/train_gating_subset_10k.json \
+    --image_folder ./playground/data/pretrain/images/train_images_10k/ \
     --mm_vision_tower "openai/clip-vit-large-patch14-336" \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer="-2,-5,-8,-11,6" \
     --mm_grid_pinpoints "'[[336, 672], [672, 336], [336, 1008], [1008, 336]]'" \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --group_by_modality_length True\
+    --group_by_modality_length True \
     --image_aspect_ratio anyres \
     --group_by_modality_length True \
     --bf16 True \
     --output_dir ./checkpoints/vis-zephyr-7b-v1-pretrain \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 8 \
+    --num_train_epochs 2 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --save_strategy "steps" \
@@ -46,4 +47,4 @@ WANDB_MODE=disabled deepspeed vis_zephyr/train/train_mem.py \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    # --report_to wandb
+    --report_to wandb
