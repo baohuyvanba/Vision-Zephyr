9bd4b1ce19d640067c1bfc621ebbf98508588da3
baohuyvanba <baohuyvanba@gmail.com>
2025-07-11 23:33:58 +0700

    Test Qformer

--- vis_zephyr/model/multimodal_projector/builder.py
+++ vis_zephyr/model/multimodal_projector/builder.py
@@ -9,16 +9,18 @@ import re
 class QFormerBlock(nn.Module):
     def __init__(self, hidden_size, nhead, ffn_dim):
         super().__init__()
-        self.self_attn = nn.MultiheadAttention(hidden_size, nhead, batch_first=True)
-        self.cross_attn = nn.MultiheadAttention(
-            num_heads=nhead,
-            embed_dim=4096,
-            kdim=5120,
-            vdim=5120,
-            batch_first=True
-        )
         self.norm1 = nn.LayerNorm(hidden_size)
+        self.self_attn = nn.MultiheadAttention(hidden_size, nhead, batch_first=True)
+
         self.norm2 = nn.LayerNorm(hidden_size)
+        self.cross_attn = nn.MultiheadAttention(
+            num_heads   = nhead,
+            embed_dim   = 4096,
+            kdim        = 5120,
+            vdim        = 5120,
+            batch_first = True
+        )
+        
         self.norm3 = nn.LayerNorm(hidden_size)
         self.ffn = nn.Sequential(
             nn.Linear(hidden_size, ffn_dim),
@@ -29,10 +31,13 @@ class QFormerBlock(nn.Module):
     def forward(self, queries, visual_features):
         q = self.norm1(queries)
         queries = queries + self.self_attn(q, q, q)[0]
+        
         q = self.norm2(queries)
         queries = queries + self.cross_attn(q, visual_features, visual_features)[0]
+        
         q = self.norm3(queries)
         queries = queries + self.ffn(q)
+        
         return queries
     
 #====================================================================================================================================
@@ -41,41 +46,47 @@ class QFormerBlock(nn.Module):
 class QFormer(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.num_queries = 16
+        self.num_queries = 32
         self.hidden_size = config.hidden_size
-        
         self.learned_queries = nn.Parameter(torch.randn(self.num_queries, self.hidden_size))
+        
+        num_blocks  = 8 
+        ffn_dim     = self.hidden_size * 4
         self.blocks = nn.ModuleList([
             QFormerBlock(
                 hidden_size = self.hidden_size,
                 nhead       = 8, #num_heads
-                ffn_dim     = 4096
+                ffn_dim     = ffn_dim #4096
             )
-            for _ in range(5)
+            for _ in range(num_blocks)
         ])
 
+        #Visual features normalization
         self.pre_norm = nn.LayerNorm(5120)
+        #Final normalization
         self.norm     = nn.LayerNorm(self.hidden_size)
 
     def forward(self, features, text_embeddings=None):
         Batch_size = features.size(0)
-        features   = self.pre_norm(features) 
+        features   = self.pre_norm(features)
         queries    = self.learned_queries.unsqueeze(0).expand(Batch_size, -1, -1)
         
         if text_embeddings is not None:
-            # if isinstance(text_embeddings, list):
-            #     text_embeddings = torch.stack(text_embeddings, dim=0)  # [B, L, D]
-            #     if text_embeddings.shape[0] < B:
-            #         repeat_times = (B + text_embeddings.shape[0] - 1) // text_embeddings.shape[0]
-            #         text_embeddings = text_embeddings.repeat((repeat_times, 1, 1))[:B]
-
             if queries.shape[1] == 1:
-                queries = torch.cat([queries, text_embeddings], dim=1)  # [B, Q+L, D]
+                init_queries = torch.cat([queries, text_embeddings], dim = 1)  # [B, Q+L, D]
 
-        for blk in self.blocks:
-            queries = blk(queries, features)
+        # for blk in self.blocks:
+        #     queries = blk(queries, features)
+        processed_inputs = self.blocks[0](
+            init_queries, features
+        )
+        queries = processed_inputs[:, :self.num_queries, :]
+        
+        for blk in self.blocks[1:]:
+                queries = blk(queries, features)
 
-        return self.norm(queries[:, :self.num_queries, :])
+        #return self.norm(queries[:, :self.num_queries, :])
+        return self.norm(queries)
 
 #====================================================================================================================================
 # Main Multimodal-Projector Builder
