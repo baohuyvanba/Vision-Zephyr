0ef58c814da3309fd8e2628943ce12f12e9ec966
nguyenthigai1905 <100947712+nguyenthigai1905@users.noreply.github.com>
2025-07-13 18:08:30 +0700

    update model base

--- script/eval/eval_mmbench.sh
+++ script/eval/eval_mmbench.sh
@@ -2,6 +2,7 @@
 
 python -m vis_zephyr.eval.eval_mmbench \
     --model-path ./checkpoints/vis-zephyr-7b-v1-pretrain/ \
+    --model-base "HuggingFaceH4/zephyr-7b-beta" \
     --question-file "./playground/data/eval/mmbench/mmbench_dev.tsv" \
     --answers-file "./playground/data/eval/mmbench/answers/mmbench_dev/zephyr-7b-beta.jsonl" \
     --conv-mode "zephyr_v1" \
